840e8f5bfcb36cb03ee7b1165938701e2f0d2e47
Stephin David <stephindavid884@gmail.com>
2026-08-05 13:55:52 +0530

    fix(backend): solved lint issues

--- backend/src/infrastructure/services/analytics/AICostEstimator.ts
+++ backend/src/infrastructure/services/analytics/AICostEstimator.ts
@@ -2,35 +2,7 @@ import { AI_PRICING } from "../../../application/constants/ai/AIPricing";
 import { EstimateAICostDTO } from "../../../application/dtos/admin/analytics/estimateAICost.dto";
 import { IAICostEstimator } from "../../../application/interfaces/services/analytics/IAICostEstimator";
 
-type PricingRule = {
-    inputPerMillionTokens: number;
-
-    outputPerMillionTokens: number;
-}
-
 export class AICostEstimator implements IAICostEstimator {
-
-    // private readonly pricing: Record<string, PricingRule> = {
-
-    //     /* Google Gemini */
-
-    //     "text-embedding-004": {
-    //         inputPerMillionTokens: 0,
-    //         outputPerMillionTokens: 0,
-    //     },
-
-    //     "gemini-2.5-flash": {
-    //         inputPerMillionTokens: 0,
-    //         outputPerMillionTokens: 0,
-    //     },
-
-    //     /* Groq */
-
-    //     "llama-3.3-70b-versatile": {
-    //         inputPerMillionTokens: 0,
-    //         outputPerMillionTokens: 0,
-    //     },
-    // };
 
     estimate(data: EstimateAICostDTO): number {
         
